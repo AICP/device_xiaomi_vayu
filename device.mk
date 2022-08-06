38590356aecb8d87a7d06bbf2ab2aaf166531cf9
nebrassy <nebras30@gmail.com>
2022-08-06 10:45:00 +0200

    vayu: remove PartsBin
    Change-Id: I96e4d1f8293f276365c0e4f00f69927adee27434

--- device.mk
+++ device.mk
@@ -424,10 +424,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
-# PartsBin
-PRODUCT_PACKAGES += \
-    PartsBin
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
